9da27f73754d7ce5f88673b2b7de17976c7f8b05
AR <achmadrifaldi64@gmail.com>
2018-05-27 07:32:49 +0700

    Fix assets calculation

--- arbitrate_bot.rb
+++ arbitrate_bot.rb
@@ -5,15 +5,29 @@ require_relative 'vip_trade'
 
 class ArbitrateBot
   def my_asset(kurs)
+    bn_ticker = Binance::Api.ticker!(symbol: 'BTCUSDT', type: 'daily')
+    bn_buy = bn_ticker[:askPrice].to_f
+
+    btcpr = bn_ticker[:askPrice].to_f * kurs
+
     binance_assets = Binance::Api.info!
     bn_usdt = binance_assets[:balances].find {|x| x[:asset] == "USDT"}[:free].to_f
+    bn_btc   =  binance_assets[:balances].find {|x| x[:asset] == "BTC"}[:free].to_f
     bn_usdt_idr = bn_usdt * kurs
+    bn_btc_idr = bn_btc * btcpr.to_f
+
+
 
     vip_btc = VipTrade.new
+    vip_ticker = vip_btc.ticker
+    vip_buy = vip_ticker["buy"].to_f
     vip_assets = vip_btc.info
-    vip_idr = vip_assets['balance']['idr']
+    vip_idr = vip_assets['balance']['idr'].to_f
+    vip_btc_blc = vip_assets['balance']['btc'].to_f
+    vip_btc_idr = vip_btc_blc * btcpr.to_f
 
-    asset = bn_usdt_idr + vip_idr
+    asset = bn_usdt_idr + bn_btc_idr + vip_idr + vip_btc_idr
+
     asset
   end
 
@@ -29,13 +43,39 @@ class ArbitrateBot
     # QTY will be set on Telegram in the future
     qty = lot
     kurs = kurs
+    profit = 0
 
     until counter == 2 do
       counter = counter + 1
 
+      bn_ticker = Binance::Api.ticker!(symbol: 'BTCUSDT', type: 'daily')
+      bn_buy = bn_ticker[:askPrice].to_f
+
+      btcpr = bn_ticker[:askPrice].to_f * kurs
+
+      binance_assets = Binance::Api.info!
+      bn_usdt = binance_assets[:balances].find {|x| x[:asset] == "USDT"}[:free].to_f
+      bn_btc   =  binance_assets[:balances].find {|x| x[:asset] == "BTC"}[:free].to_f
+      bn_usdt_idr = bn_usdt * kurs
+      bn_btc_idr = bn_btc * btcpr.to_f
+
+      vip_btc = VipTrade.new
+      vip_ticker = vip_btc.ticker
+      vip_buy = vip_ticker["buy"].to_f
+      vip_assets = vip_btc.info
+      vip_idr = vip_assets['balance']['idr'].to_f
+      vip_btc_blc = vip_assets['balance']['btc'].to_f
+      vip_btc_idr = vip_btc_blc * btcpr.to_f
+
+
       text += "<b>Try #{counter}</b>\n\n"
 
       initial_asset = my_asset(kurs)
+
+      text += "<b>Initial Asset</b>:Bn  Idr #{bn_usdt_idr.to_i}\n"
+      text += "<b>Initial Asset</b>:Bn  Btc #{bn_btc.round(4)}\t\t#{bn_btc_idr.to_i}\n"
+      text += "<b>Initial Asset</b>:Vip Idr #{vip_idr.to_i}\n"
+      text += "<b>Initial Asset</b>:Bn Btc #{vip_btc_blc.round(4)}\t\t#{vip_btc_idr.to_i}\n"
       text += "<b>Initial Asset</b>: #{initial_asset.to_i}\n\n"
 
       vip_btc = VipTrade.new
@@ -55,20 +95,24 @@ class ArbitrateBot
 
       bn_high = bn_ticker[:highPrice]
       bn_low = bn_ticker[:lowPrice]
-      bn_buy = bn_ticker[:askPrice].to_f
-      bn_sell = bn_ticker[:bidPrice].to_f
+      bn_buy = bn_ticker[:bidPrice].to_f
+      bn_sell = bn_ticker[:askPrice].to_f
 
       text += "<b>BINANCE BTC</b>\n"
       text += "buy: #{bn_buy}\n"
       text += "sell: #{bn_sell}\n\n"
 
-      vip_buyUsd = (vip_buy / kurs)
-      vip_sellUsd = (vip_sell / kurs)
+      vip_buyUsd = (vip_buy / kurs).to_f
+      vip_sellUsd = (vip_sell / kurs).to_f
 
-      puts "VIP #{vip_buyUsd}"
+      puts "VIP Buy #{vip_buyUsd}"
+      puts "VIP SELL #{vip_sellUsd}"
 
-      bnbuy_vipsell = (((bn_sell - vip_buyUsd) / bn_sell) * 100).round(3)
-      vipbuy_bnsell = (((vip_sellUsd - bn_buy) /vip_sellUsd) * 100).round(3)
+
+      bnbuy_vipsell = (((vip_buyUsd / bn_sell) - 1) * 100).round(3)
+
+      vipbuy_bnsell = (((bn_buy / vip_sellUsd) - 1) * 100).round(3)
+
 
       text += "<b>SPREAD BUY BINANCE -> VIP SELL</b>\n"
       text += "#{bnbuy_vipsell}\n\n"
@@ -83,9 +127,10 @@ class ArbitrateBot
         # Buy 15$ BTC
         Binance::Api::Order.create!(symbol: 'BTCUSDT', side: 'BUY', type: 'LIMIT', price: bn_sell, quantity: qty, timeInForce: 'GTC')
 
-        current_asset = my_asset(kurs).to_i
+        current_asset = my_asset(kurs).to_f
         text += "<b>Current Asset: #{current_asset}</b>\n"
         text += "<b>Profit: #{current_asset - initial_asset}</b>\n\n"
+
       elsif vipbuy_bnsell >= spread
         # Sell in VIP
         vip_btc.buy(qty)
@@ -93,9 +138,8 @@ class ArbitrateBot
         # Buy 15$ BTC
         Binance::Api::Order.create!(symbol: 'BTCUSDT', side: 'SELL', type: 'LIMIT', price: bn_sell, quantity: qty, timeInForce: 'GTC')
 
-        current_asset = my_asset(kurs).to_i
+        current_asset = my_asset(kurs).to_f
         text += "<b>Current Asset: #{current_asset}</b>\n"
-        text += "<b>Profit: #{current_asset - initial_asset}</b>\n\n"
       end
 
       sleep 5
@@ -115,6 +159,7 @@ class ArbitrateBot
       text += "<b>BINANCE BALANCE</b>\n"
       text += "USDT: #{bn_usdt}\n"
       text += "BTC: #{bn_btc}\n"
+
     rescue => e
       text += "<b>Failed to get BINANCE Info</b>\n"
       text += e.inspect.to_s
